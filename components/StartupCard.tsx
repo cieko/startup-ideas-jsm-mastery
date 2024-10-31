4546731b2ab4894ece1a99cfffd01485c2598e81
cieko <rahulsaxena2021@hotmail.com>
2024-10-31 14:43:45 +0530

    sanity schema

---
+++ components/StartupCard.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-img-element */
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    description,
+    author: { _id: authorId, name },
+    title,
+    category,
+    _id,
+    image,
+  } = post;
+
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-JHbMBsZcAXmA3R8jdYIY2xFVG5pJ-AiCtw&s"
+            }
+            alt="avatar"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        <img
+          src={image}
+          alt="placeholders"
+          className="startup-card_img !object-[0px_-40px]"
+        />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
