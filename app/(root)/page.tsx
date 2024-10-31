d0d7db843ac54f31eb38b6908c5277dbfd86b67e
cieko <rahulsaxena2021@hotmail.com>
2024-10-31 22:05:14 +0530

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 // import { client } from "@/sanity/lib/client";
@@ -11,6 +12,9 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session = await auth()
+  console.log(session?.id)
 
   // const posts = await client.fetch(STARTUPS_QUERY); // this was old method that uses cache or cdn and needs to revalidate that is managed in the sanity -> lib -> client.ts
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params }); // to live update the results
